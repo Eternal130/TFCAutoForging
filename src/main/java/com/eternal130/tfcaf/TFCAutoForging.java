cf65c071929748bae5971d4494ab1dc1ba737f8b
Eternal130 <111716703+Eternal130@users.noreply.github.com>
2024-04-12 13:56:34 +0800

    init

---
+++ src/main/java/com/eternal130/tfcaf/TFCAutoForging.java
@@ -0,0 +1,31 @@
+package com.eternal130.tfcaf;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = TFCAutoForging.MODID, name = TFCAutoForging.NAME, version = TFCAutoForging.VERSION)
+public class TFCAutoForging
+{
+    public static final String MODID = "tfcaf";
+    public static final String NAME = "TFC Auto Forging";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
